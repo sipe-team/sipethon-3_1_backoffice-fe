0ca79632a705c389af11c0711d55c01de4352941
kimdaeyeobbb <kimdyk2@hanmail.net>
2025-01-18 21:10:12 +0900

    feat: table row 클릭시 상세 페이지로 이동하도록 축

--- app/components/member/list/TableSection/TableSection.tsx
+++ app/components/member/list/TableSection/TableSection.tsx
@@ -1,14 +1,5 @@
 import { useState } from 'react';
 import {
-  Table,
-  TableCaption,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
-} from '~/components/ui/table';
-import {
   Pagination,
   PaginationContent,
   PaginationItem,
@@ -18,16 +9,20 @@ import {
   PaginationEllipsis,
 } from '~/components/ui/pagination';
 
+import { useNavigate } from 'react-router-dom';
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from '~/components/ui/table';
+
 import type { MemberInfo } from 'app/components/member/list/TableSection/MemberInfo';
 
 export default function TableSection({ memberInfo }: { memberInfo: MemberInfo[] }) {
-  /* 회원상태 관리 */
-  const getMemberStatus = (member: MemberInfo) => {
-    if (member.isDropped) {
-      return '제명 회원';
-    }
-    return member.isStaff ? '운영진' : '일반 회원';
-  };
+  const navigate = useNavigate();
 
   /* 페이징 */
   const [currentPage, setCurrentPage] = useState(1);
@@ -59,64 +54,70 @@ export default function TableSection({ memberInfo }: { memberInfo: MemberInfo[] 
     return items;
   };
 
+  const getMemberStatus = (member: MemberInfo) => {
+    if (member.isDropped) {
+      return '제명 회원';
+    }
+    return member.isStaff ? '운영진' : '일반 회원';
+  };
+
+  const handleRowClick = (id: number) => {
+    navigate(`/member/${id}`);
+  };
+
   return (
-    <>
-      {memberInfo.length > 0 && (
-        <div className="pb-10 text-black">
-          <Table className="text-center">
-            {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-            <TableHeader className="bg-slate-100">
-              <TableRow>
-                <TableHead className="text-center">고유번호</TableHead>
-                <TableHead className="text-center">이름</TableHead>
-                <TableHead className="text-center">상태</TableHead>
-                <TableHead className="text-center">직군</TableHead>
-                <TableHead className="text-center">거주지</TableHead>
-                <TableHead className="text-center">생년월일</TableHead>
-                <TableHead className="text-center">이메일</TableHead>
-                <TableHead className="text-center">연락처</TableHead>
-              </TableRow>
-            </TableHeader>
+    <div className="pb-10">
+      <Table className="text-center">
+        <TableHeader className="bg-slate-100">
+          <TableRow>
+            <TableHead className="text-center">고유번호</TableHead>
+            <TableHead className="text-center">이름</TableHead>
+            <TableHead className="text-center">상태</TableHead>
+            <TableHead className="text-center">직군</TableHead>
+            <TableHead className="text-center">거주지</TableHead>
+            <TableHead className="text-center">생년월일</TableHead>
+            <TableHead className="text-center">이메일</TableHead>
+            <TableHead className="text-center">연락처</TableHead>
+          </TableRow>
+        </TableHeader>
 
-            <TableBody className="bg-white">
-              {currentMembers.map((member) => (
-                <TableRow key={member.id}>
-                  <TableCell>{member.id}</TableCell>
-                  <TableCell>{member.name}</TableCell>
-                  <TableCell>{getMemberStatus(member)}</TableCell>
-                  <TableCell>{member.field}</TableCell>
-                  <TableCell>{member.region}</TableCell>
-                  <TableCell>{member.birth}</TableCell>
-                  <TableCell>{member.email}</TableCell>
-                  <TableCell>{member.phone}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-
-          {/* 페이징 */}
-          <div className="flex justify-center mt-4">
-            <Pagination>
-              <PaginationContent>
-                {currentPage > 1 && (
-                  <PaginationItem>
-                    <PaginationPrevious
-                      href="#"
-                      onClick={() => handlePageChange(currentPage - 1)}
-                    />
-                  </PaginationItem>
-                )}
-                {renderPaginationItems()}
-                {currentPage < totalPages && (
-                  <PaginationItem>
-                    <PaginationNext href="#" onClick={() => handlePageChange(currentPage + 1)} />
-                  </PaginationItem>
-                )}
-              </PaginationContent>
-            </Pagination>
-          </div>
-        </div>
-      )}
-    </>
+        <TableBody className="bg-white">
+          {memberInfo.map((member) => (
+            <TableRow
+              key={member.id}
+              onClick={() => handleRowClick(member.id)}
+              className="cursor-pointer"
+            >
+              <TableCell>{member.id}</TableCell>
+              <TableCell>{member.name}</TableCell>
+              <TableCell>{getMemberStatus(member)}</TableCell>
+              <TableCell>{member.field}</TableCell>
+              <TableCell>{member.region}</TableCell>
+              <TableCell>{member.birth}</TableCell>
+              <TableCell>{member.email}</TableCell>
+              <TableCell>{member.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {/* 페이징 */}
+      <div className="flex justify-center mt-4">
+        <Pagination>
+          <PaginationContent>
+            {currentPage > 1 && (
+              <PaginationItem>
+                <PaginationPrevious href="#" onClick={() => handlePageChange(currentPage - 1)} />
+              </PaginationItem>
+            )}
+            {renderPaginationItems()}
+            {currentPage < totalPages && (
+              <PaginationItem>
+                <PaginationNext href="#" onClick={() => handlePageChange(currentPage + 1)} />
+              </PaginationItem>
+            )}
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </div>
   );
 }
