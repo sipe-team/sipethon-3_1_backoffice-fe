6e6a0cefe0ecd6c026535728f4cc533971999626
synuns <identityjang@gmail.com>
2025-01-18 20:55:04 +0900

    feat: faq, 지원 자격 get

--- app/components/phase/FaqListCard.tsx
+++ app/components/phase/FaqListCard.tsx
@@ -15,35 +15,33 @@ type FaqListCardProps = {
   faqs: Faq[];
 };
 
-const mockFaqListCard: FaqListCardProps = {
-  faqs: [
-    { question: '3기 선발 기준은 어떻게 되나요?', answer: '답변1' },
-    {
-      question: '현재 개발자로 일은 안하고 있지만 개발 경력이 있는데 지원 가능한가요?',
-      answer: '답변2',
-    },
-    {
-      question: '수도권에 거주하고 있지 않지만 주요 활동 지역은 수도권인데 활동을 할 수 있나요?',
-      answer: '답변3',
-    },
-    {
-      question: '3기 선발 인원은 몇명인가요?',
-      answer: '답변4',
-    },
-    {
-      question: '미성년자이지만 개발자로 근무하고 있는데 지원할 수 있나요?',
-      answer: '답변4',
-    },
-    {
-      question: '활동 시간은 언제인가요?',
-      answer: '답변5',
-    },
-  ],
-};
+// const mockFaqListCard: FaqListCardProps = {
+//   faqs: [
+//     { question: '3기 선발 기준은 어떻게 되나요?', answer: '답변1' },
+//     {
+//       question: '현재 개발자로 일은 안하고 있지만 개발 경력이 있는데 지원 가능한가요?',
+//       answer: '답변2',
+//     },
+//     {
+//       question: '수도권에 거주하고 있지 않지만 주요 활동 지역은 수도권인데 활동을 할 수 있나요?',
+//       answer: '답변3',
+//     },
+//     {
+//       question: '3기 선발 인원은 몇명인가요?',
+//       answer: '답변4',
+//     },
+//     {
+//       question: '미성년자이지만 개발자로 근무하고 있는데 지원할 수 있나요?',
+//       answer: '답변4',
+//     },
+//     {
+//       question: '활동 시간은 언제인가요?',
+//       answer: '답변5',
+//     },
+//   ],
+// };
 
-// function FaqListCard({ faqs }: FaqListCardProps) {
-function FaqListCard() {
-  const { faqs } = mockFaqListCard;
+function FaqListCard({ faqs }: FaqListCardProps) {
   return (
     <Card title="FAQ" onEdit={() => {}}>
       <Accordion type="single" collapsible>
