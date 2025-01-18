d22680eb9d881d868b8d7f237e2490870365a7be
hy57in <gywls00100@gmail.com>
2025-01-18 19:30:40 +0900

    feat: 출석관리 페이지 구현

--- app/types/attendance.ts
+++ app/types/attendance.ts
@@ -16,3 +16,34 @@ export interface PointHistory {
   point: string;
   reason: string;
 }
+
+export interface AttendanceType {
+  term?: number | null; //기수
+  phase?: number | null; //회차
+  date?: string | null; //회차 날짜
+  content?: string | null; //내용
+  startTime?: string | null; //시작 시간 25.01.11 14:00
+  lateTime?: string | null; //지각 시간
+  absenceTime?: string | null; //결석 시간
+}
+
+export interface GetPhaseRequest {
+  term: number;
+}
+
+export interface GetPhaseResponse {
+  phase: number;
+  date: string; // "25.01.11" 형식
+}
+
+export interface GenerateQRCodeRequest {
+  term: number;
+  phase: number;
+  startTime: string; // "25.01.11 14:00" 형식
+  lateTime: string;
+  absenceTime: string;
+}
+
+export interface GenerateQRCodeResponse {
+  imageUrl: string;
+}
