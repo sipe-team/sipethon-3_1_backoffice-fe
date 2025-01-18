f43a5eb38fa83fba8f41e448068441abf5d61a89
synuns <identityjang@gmail.com>
2025-01-18 20:03:20 +0900

    feat: Phase 선택 selector 추가

--- app/pages/BasicInfo.tsx
+++ app/pages/BasicInfo.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import ContentContainer from '~/components/basicInfo/ContentContainer';
 import MainInfo from '~/components/basicInfo/MainInfo';
 import ApplicationDeadlineCard from '~/components/phase/ApplicationDeadlineCard';
 import FaqListCard from '~/components/phase/FaqListCard';
+import HeaderedContentContainer from '~/components/phase/HeaderedContentContainer';
+import PhaseSelector from '~/components/phase/PhaseSelector';
 import ProgramScheduleCard from '~/components/phase/ProgramScheduleCard';
 import RequirementsCard from '~/components/phase/RequirementsCard';
 
+const phaseList = [
+  { label: '1기', value: '1' },
+  { label: '2기', value: '2' },
+  { label: '3기', value: '3' },
+];
+
 export default function BasicInfo() {
+  const [phase, setPhase] = useState<string>(phaseList[0].value);
+
   return (
     <div className="bg-slate-50">
       <MainInfo />
@@ -18,14 +29,24 @@ export default function BasicInfo() {
           </p>
         </div>
       </ContentContainer>
-      <ContentContainer title="기수별 정보">
+      <HeaderedContentContainer
+        title="기수별 정보"
+        header={
+          <PhaseSelector
+            phaseList={phaseList}
+            value={phase}
+            onChange={setPhase}
+            onRegister={() => {}}
+          />
+        }
+      >
         <div className="grid grid-cols-2 gap-4">
           <ApplicationDeadlineCard applicationDeadline={[]} />
           <RequirementsCard />
           <ProgramScheduleCard />
           <FaqListCard />
         </div>
-      </ContentContainer>
+      </HeaderedContentContainer>
     </div>
   );
 }
